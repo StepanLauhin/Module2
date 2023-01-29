5c58b3c7009afe03ae6b378e7f2015c274884839
StepanLauhin <115322133+StepanLauhin@users.noreply.github.com>
2023-01-29 20:18:28 +0300

    Лабораторная 3

--- Лабораторная 3/main.py
+++ Лабораторная 3/main.py
@@ -1,31 +1,59 @@
 class Book:
     """ Базовый класс книги. """
     def __init__(self, name: str, author: str):
-        self.name = name
-        self.author = author
+        self._name = name
+        self._author = author
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def author(self):
+        return self._author
 
     def __str__(self):
-        return f"Книга {self.name}. Автор {self.author}"
+        return f"Книга {self._name}. Автор {self._author}"
 
     def __repr__(self):
-        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
+        return f"{self.__class__.__name__}(name={self._name!r}, author={self._author!r})"
 
 
-class PaperBook:
+class PaperBook(Book):
     def __init__(self, name: str, author: str, pages: int):
-        self.name = name
-        self.author = author
-        self.pages = pages
+        super().__init__(name, author)
+        self._pages = self.pages(pages)
 
-    def __str__(self):
-        return f"Книга {self.name}. Автор {self.author}"
+    @property
+    def pages(self):
+        return self._pages
+
+    @pages.setter
+    def pages(self, new_pages: int):
+        if not isinstance(new_pages, int):
+            raise TypeError("Значение количества страниц должно быть типа int")
+        if new_pages <= 0:
+            raise ValueError("Значение количества страниц должно быть положительным числом")
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self._name!r}, author={self._author!r}, pages={self._pages!r})"
 
 
-class AudioBook:
+class AudioBook(Book):
     def __init__(self, name: str, author: str, duration: float):
-        self.name = name
-        self.author = author
-        self.duration = duration
+        super().__init__(name, author)
+        self._duration = self.duration(duration)
 
-    def __str__(self):
-        return f"Книга {self.name}. Автор {self.author}"
+    @property
+    def duration(self):
+        return self._duration
+
+    @duration.setter
+    def duration(self, new_duration: float):
+        if not isinstance(new_duration, float):
+            raise TypeError("Значение продолжительности должно быть типа float")
+        if new_duration <= 0:
+            raise ValueError("Значение продолжительности должно быть положительным числом")
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self._name!r}, author={self._author!r}, pages={self._duration!r})"
